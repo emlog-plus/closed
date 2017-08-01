d837e2623d787e0faa96686c1c93f99d7afb890e
Flyerme <gao.eison@gmail.com>
2017-08-01 13:45:30 -0400

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,25 @@
+<?php 
+!defined('EMLOG_ROOT') && exit('access deined!');
+
+$lang = array(
+
+"closed"=>"Close Blog",
+"home" =>"Home",
+"blog_set"=>"Blog status settings",
+"blog_info"=>"Tip: please make sure this plug-in directory and the closed and closed files are read and write permissions (777).",
+"blog_state_"=>"Blog status:",
+"state_off"=>"Open, all users can access the blog normally",
+"state_on"=>"Shut down, the Fei cannot access the blog",
+"null"=>"Null and void",
+"mobile_state_"=>"Mobile status:",
+"mobile_off"=>"Yes",
+"mobile_on"=>"No",
+"close_reason"=>"Blog closing reasons:",
+"close_because"=>"something.....",
+"close_btn"=>"Save settings",
+"close_error"=>"The operation failed, please make sure that the plug-in directory",
+"blog_set_ok"=>"Save success",
+"answer_blog"=>"Are you close the blog.:",
+"answer_mobile"=>"Are you close the mobile blog:",
+
+);
