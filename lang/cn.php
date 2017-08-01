0d18eff06226d601cec14607a63ceaa1143cfcf7
Flyerme <gao.eison@gmail.com>
2017-08-01 13:45:04 -0400

    Create cn.php

---
+++ lang/cn.php
@@ -0,0 +1,24 @@
+<?php 
+!defined('EMLOG_ROOT') && exit('access deined!');
+$lang = array(
+
+"closed"=>"关闭博客",
+"home" =>"首页",
+"blog_set"=>"博客状态设置",
+"blog_info"=>"提示：请确保本插件目录及closed_config.php文件据有读写权限(777).",
+"blog_state_"=>"博客状态为:",
+"state_off"=>"所有用户可正常访问博客",
+"state_on"=>"非管理员无法访问博客",
+"null"=>"无效",
+"mobile_state_"=>"手机版状态为:",
+"mobile_off"=>"是",
+"mobile_on"=>"否",
+"close_reason"=>"博客关闭原因说明:",
+"close_because"=>"博客维护中......暂停访问",
+"close_btn"=>"保存设置",
+"close_error"=>"操作失败，请确保插件目录(/content/plugins/closed-master/)可写",
+"blog_set_ok"=>"保存成功",
+"answer_blog"=>"是否关闭博客访问:",
+"answer_mobile"=>"否允许手机版访问:",
+
+);
